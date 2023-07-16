5e35d601257cdbe869f114f8a17a4bda33326091
Vitalii Fynytskyi <fynytsky01@gmail.com>
2023-07-16 13:58:06 +0300

    Add project files.

---
+++ DatabaseBackupper/GoogleDriveService.cs
@@ -0,0 +1,85 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
+namespace DatabaseBackupper
+{
+    public class GoogleDriveService
+    {
+        private static DriveService service;
+        static string[] Scopes = { DriveService.Scope.Drive };
+        static string ApplicationName = "DatabaseBackupper";
+        public static void Authenticate()
+        {
+            UserCredential credential;
+
+            using (var stream = new FileStream("GmailApiCredentials2.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.FromStream(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+            }
+
+            // Create Drive API service.
+            service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+        }
+        public static string UploadFile(string driveFileName, string description, string mimeType, string pathToLocalFile)
+        {
+            var existingFile = FindFileByName(driveFileName);
+
+            var fileMetadata = new Google.Apis.Drive.v3.Data.File()
+            {
+                Name = driveFileName,
+                Description = description,
+                MimeType = mimeType
+            };
+
+            FilesResource.CreateMediaUpload request;
+
+            using (var stream = new System.IO.FileStream(pathToLocalFile, System.IO.FileMode.Open))
+            {
+                if (existingFile != null)
+                {
+                    // If file exists, delete the file.
+                    var deleteRequest = service.Files.Delete(existingFile.Id);
+                    deleteRequest.Execute();
+                }
+
+                // Create a new file.
+                request = service.Files.Create(fileMetadata, stream, mimeType);
+                request.Fields = "id";
+                request.Upload();
+            }
+
+            var file = request.ResponseBody;
+            return file.Id;
+        }
+
+        private static Google.Apis.Drive.v3.Data.File FindFileByName(string name)
+        {
+            var request = service.Files.List();
+            request.Q = $"name='{name}'";
+            request.Fields = "files(id, name)";
+            var result = request.Execute();
+
+            foreach (var file in result.Files)
+            {
+                if (file.Name == name)
+                {
+                    return file;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ DatabaseBackupper/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Data.SqlClient;
+namespace DatabaseBackupper
+{
+    class Program
+    {
+        static void Main()
+        {
+            ///read ini file
+            string serverName = IniService.GetPrivateString("Options", "ServerName");
+            string databaseName = IniService.GetPrivateString("Options", "DatabaseName");
+            string databaseBackupPath = IniService.GetPrivateString("Options", "DatabaseBackupPath");
+            
+            string connectionString = $"Data Source={serverName};Initial Catalog={databaseName};Integrated Security=true;Encrypt=false;TrustServerCertificate=true";
+            ///perform backup
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand sqlCommand= connection.CreateCommand();
+                sqlCommand.CommandText = $"BACKUP DATABASE {connection.Database} TO DISK='{databaseBackupPath}' WITH INIT";
+                sqlCommand.ExecuteNonQuery();
+            }
+            ///upload to google drive
+            GoogleDriveService.Authenticate();
+            string fileID = GoogleDriveService.UploadFile($"{databaseName}.bak", "Database backup", "application/octet-stream", databaseBackupPath);
+        }
+    }
+}
