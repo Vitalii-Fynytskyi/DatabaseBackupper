0d39b480a8f0302abbaedad43b7c7b0faa037c42
Vitalii Fynytskyi <fynytsky01@gmail.com>
2023-07-18 23:08:43 +0300

    Added user with password. Changed from Microsoft.Data.SqlClient to System.Data.SqlClient. Added error logging

--- DatabaseBackupper/Program.cs
+++ DatabaseBackupper/Program.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using System.Data.SqlClient;
 namespace DatabaseBackupper
 {
     class Program
@@ -9,13 +9,16 @@ namespace DatabaseBackupper
             string serverName = IniService.GetPrivateString("Options", "ServerName");
             string databaseName = IniService.GetPrivateString("Options", "DatabaseName");
             string databaseBackupPath = IniService.GetPrivateString("Options", "DatabaseBackupPath");
-            
-            string connectionString = $"Data Source={serverName};Initial Catalog={databaseName};Integrated Security=true;Encrypt=false;TrustServerCertificate=true";
+            string databaseUserId = IniService.GetPrivateString("Options", "DatabaseUserId");
+            string databaseUserPassword = IniService.GetPrivateString("Options", "DatabaseUserPassword");
+
+            string connectionString = $"Data Source={serverName};Initial Catalog={databaseName};User Id ={databaseUserId}; Password={databaseUserPassword}";
+
             ///perform backup
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
-                SqlCommand sqlCommand= connection.CreateCommand();
+                SqlCommand sqlCommand = connection.CreateCommand();
                 sqlCommand.CommandText = $"BACKUP DATABASE {connection.Database} TO DISK='{databaseBackupPath}' WITH INIT";
                 sqlCommand.CommandTimeout = 3600; // This will set the timeout to 1 hour
                 sqlCommand.ExecuteNonQuery();
